4762c216262425736c9a4bad9a3e27064363393a
upsidedown <usidedown@gmail.com>
2012-07-05 17:17:43 +0300

    Updated URL

--- NiniteManager/project.clj
+++ NiniteManager/project.clj
@@ -1,6 +1,6 @@
 (defproject NiniteManager "0.1.0-SNAPSHOT"
   :description "This is a simple program to keep track of your ninite applications"
-  :url "http://example.com/FIXME"
+  :url "https://code.google.com/p/ninite-manager"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]])
